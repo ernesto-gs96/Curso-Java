4d5cfe792889ac17e55625a6f1a8a0919ca93f89
ernesto-gs96 <ernesto_gs96@hotmail.com>
2024-03-19 12:37:34 -0600

    Strings

---
+++ Curso Profesional de JAVA/src/Strings/Cadena.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class Cadena {
+    public static void main(String[] args) {
+        /*
+         * Un String es un conjunto o cadena de carateres y no es tipo de dato primitivo,
+         * siendo una clase en JAVA
+         */
+
+        String mensaje = "Soy un String";
+        System.out.println(mensaje);
+
+        // Como es una clase de igual modo se puede declarar de la siguiente manera
+        String mensaje2 = new String("Tambien soy un String");
+        System.out.println(mensaje2);
+    }
+}
