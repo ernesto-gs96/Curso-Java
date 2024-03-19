95d1b7ce3d81458391021e42214a6bb47a032691
ernesto-gs96 <ernesto_gs96@hotmail.com>
2024-03-19 12:28:31 -0600

    Operador ternario

---
+++ Curso Profesional de JAVA/src/OperadorTernario.java
@@ -0,0 +1,17 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        final int edadPermitida = 18;
+        int edad = 19;
+        String mensaje;
+
+        /*
+         * Operador ternario
+         * expresion1 ? expresion2 : expresion3
+         * Si expresion1 es verdadero se ejecuta la expresion2
+         * Si expresion1 es falsa se ejecuta la expresion3
+         */
+        mensaje = edad >= edadPermitida ? "Tiene la edad permitida" : "No tiene la edad permitida";
+
+        System.out.println(mensaje);
+    }
+}
