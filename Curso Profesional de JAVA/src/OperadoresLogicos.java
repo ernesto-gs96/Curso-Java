16d70cf18eb6838df17f2331c02912dbeb3fc276
ernesto-gs96 <ernesto_gs96@hotmail.com>
2024-03-19 12:23:08 -0600

    Operadores logicos

---
+++ Curso Profesional de JAVA/src/OperadoresLogicos.java
@@ -0,0 +1,40 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        int numero1 = 5, numero2 = 5;
+        boolean resultado, aux;
+
+        // AND (Y) , Todas las comparaciones tiene que ser verdaderas (true)
+        aux = true;
+        resultado = numero1 >= numero2 && aux;
+        System.out.println(numero1 + " >= " + numero2 + " && " + aux + " : " + resultado);
+
+        aux = false;
+        resultado = numero1 >= numero2 && aux;
+        System.out.println(numero1 + " >= " + numero2 + " && " + aux + " : " + resultado);
+
+
+        // OR (O) , Por lo menos una cmparacion tiene que ser verdadera (true)
+        aux = true;
+        resultado = numero1 >= numero2 || aux;
+        System.out.println(numero1 + " >= " + numero2 + " || " + aux + " : " + resultado);
+
+        aux = false;
+        resultado = numero1 >= numero2 || aux;
+        System.out.println(numero1 + " >= " + numero2 + " || " + aux + " : " + resultado);
+
+
+        // Se pueden usar los operadores logicos en conjunto
+        aux = true;
+        resultado = (numero1 >= numero2 || aux) && (numero1 >= numero2 && aux);
+        System.out.println(resultado);
+
+
+        // NOT (no) , Negar o cambiar el booleano
+        aux = false;
+        resultado = !(numero1 >= numero2 && aux);
+        System.out.println( "!( " + numero1 + " >= " + numero2 + " && " + aux + " ) : " + resultado);
+
+        resultado = !(numero1 >= numero2 || aux);
+        System.out.println( "!( " + numero1 + " >= " + numero2 + " || " + aux + " ) : " + resultado);
+    }
+}
