8effcfd68abbdbd86ad97499b2c01906b45c0fcd
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-04-07 12:53:57 -0600

    Organizacion de carpetas

--- 02-Máster Completo en Java de cero a experto 2024/11_Linea_de_comando/src/ArgumentosLineaComando.java
+++ 02-Máster Completo en Java de cero a experto 2024/11_Linea_de_comando/src/ArgumentosLineaComando.java
@@ -6,7 +6,7 @@ public class ArgumentosLineaComando {
             System.exit(-1);
         }
         for(int i = 0; i < args.length; i++){
-            System.out.println("Argumentos nº " + i + ": " + args[i]);
+            System.out.println("Argumentos n° " + i + ": " + args[i]);
         }
     }
 }
